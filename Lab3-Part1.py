ddcfcf1f1a191edb68ea3073a84e6379fa467d55
dermarkr <45435121+dermarkr@users.noreply.github.com>
2020-09-28 15:11:05 -0400

    Part 1 Complete

--- Lab3-Part1.py
+++ Lab3-Part1.py
@@ -1,14 +1,23 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
 
 app = Flask(__name__)
-
+moment = Moment(app)
 bootstrap = Bootstrap(app)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', current_time=datetime.utcnow())
 
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html', name=name)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+if __name__ == '__main__':
+    app.run(debug=True)
