2073806946327759608c72ec1253fd72afa9ae9b
dermarkr <45435121+dermarkr@users.noreply.github.com>
2020-09-30 10:52:24 -0400

    Lab3_Part2 Final
    Only issue with this is that it pulls cached data from the browser. To make it show correct text on opening cached data must be cleared.

--- Lab3-Part1.py
+++ Lab3-Part1.py
@@ -18,24 +18,19 @@ bootstrap = Bootstrap(app)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    session.pop('name', url_for('index'))
-    # session.clear()
     form = NameForm()
     if form.validate_on_submit():
         old_name = session.get('name')
         if(old_name is not None and old_name != form.name.data):
             flash('Looks like you have changed your name!')
         session['name'] = form.name.data
-        # return redirect(url_for('index'))
         old_email = session.get('email')
         if(old_email is not None and old_email != form.email.data):
             flash('Looks like you have changed your email!')
         session['email'] = form.email.data
         form.name.data = ''
         form.email.data = ''
-        # session.pop('name', None)
         return redirect(url_for('index'))
-
     return render_template('index.html', form=form, name=session.get('name'), email=session.get('email'))
 
 @app.route('/user/<name>')
